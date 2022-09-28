1da0c1d4ebb48cb2bd2bc9f7df97e9d0731eedda
DANK1NG <nikita.tsepochko20@gmail.com>
2022-09-28 23:49:07 +0300

    Update custom_storages.py

--- config/custom_storages.py
+++ config/custom_storages.py
@@ -1,7 +1,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StatisStorage(S3Boto3Storage):
+class StaticStorage(S3Boto3Storage):
     location = "static/"
     file_overwrite = False
 
