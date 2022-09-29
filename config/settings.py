a756c0ad1cb16d2e7232eb51824ac2a2d0f3023e
DANK1NG <nikita.tsepochko20@gmail.com>
2022-09-29 11:41:23 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -186,6 +186,9 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-nomad"
+    AWS_BUCKET_ACL = "public-read"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-2.amazonaws.com"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
